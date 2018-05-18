6baaf100060f55138b08c4cb1d760025e07c61c4
Tullio Benzi <tubenzi@gmail.com>
2018-05-18 18:25:11 +0200

    update

--- src/NewWordClockDST/NewWordClockDST.ino
+++ src/NewWordClockDST/NewWordClockDST.ino
@@ -21,6 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 // ================================================================================
+//
 // Scopo
 // visualizzazione dell'ora mendiante scritte che vengono illuminate dall'accensione
 // di led sottostanti:
